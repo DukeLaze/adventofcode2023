0605fb15fe18196172c214ca4f21afe4efbf418f
Lasse Seivaag <lasse_0407@hotmail.com>
2023-12-11 20:23:33 +0100

    Day2 P1

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,12 +1,14 @@
+#[derive(Debug)]
 struct Draw {
     red: u32,
     green: u32,
-    blue: u32
+    blue: u32,
 }
 
+#[derive(Debug)]
 struct Game {
     id: u32,
-    draws: Vec<Draw>
+    draws: Vec<Draw>,
 }
 
 fn parse_input(input: &str) -> Vec<Game> {
@@ -21,23 +23,23 @@ fn parse_input(input: &str) -> Vec<Game> {
 
     // Parse each line
     for line in lines {
+        // ["Game X", "Draws colors;*]
         let parts = line.split(':').collect::<Vec<&str>>();
         assert_eq!(parts.len(), 2);
         // Get Game ID
         // Game X
         let id = parts[0].split_whitespace().nth(1).unwrap().trim();
-        println!("id: {}", id);
         let id = id.parse::<u32>().unwrap();
 
         // Get Draws
+        // ["Draws Colors,*"]
         let parts = parts[1].split(';').collect::<Vec<&str>>();
-        assert_eq!(parts.len(), 3);
 
+        let mut draws: Vec<Draw> = Vec::new();
         for draw in parts {
-            let mut draws: Vec<Draw> = Vec::new();
             let parts = draw.split(',').collect::<Vec<&str>>();
             // May be ',' or 'x color'. Does not need to contain all colors
-            if parts.len() > 0 && parts.len() % 2 == 0 {
+            if parts.len() > 0 {
                 let mut draw = Draw {
                     red: 0,
                     green: 0,
@@ -45,36 +47,45 @@ fn parse_input(input: &str) -> Vec<Game> {
                 };
 
                 // Take number and color
-                let parts = parts.chunks(2).collect::<Vec<&[&str]>>();
+                let parts = parts
+                    .into_iter()
+                    .map(|part| part.trim())
+                    .collect::<Vec<&str>>();
                 for i in 0..parts.len() {
-                    let color = parts[i][1];
-                    let num = parts[i][0].parse::<u32>().unwrap();
-                    if color == "red" { draw.red = num }
-                    else if color == "green" { draw.green = num }
-                    else if color == "blue" { draw.blue = num }
+                    // count color
+                    let parts = parts[i].split(' ').collect::<Vec<&str>>();
+                    let num = parts[0].parse::<u32>().unwrap();
+                    let color = parts[1];
+                    if color == "red" {
+                        draw.red = num
+                    } else if color == "green" {
+                        draw.green = num
+                    } else if color == "blue" {
+                        draw.blue = num
+                    }
                 }
-            }
-            else {
+                draws.push(draw);
+            } else {
                 draws.push(Draw {
                     red: 0,
                     green: 0,
                     blue: 0,
                 })
             }
-
+        }
         games.push(Game { id, draws });
-    }
     }
     games
 }
-
 
 fn is_legal_game(game: &Game) -> bool {
     const MAX_RED: u32 = 12;
     const MAX_GREEN: u32 = 13;
     const MAX_BLUE: u32 = 14;
-    
-    game.draws.iter().all(|draw| draw.red <= MAX_RED && draw.green <= MAX_GREEN && draw.blue <= MAX_BLUE)
+
+    game.draws
+        .iter()
+        .all(|draw| draw.red <= MAX_RED && draw.green <= MAX_GREEN && draw.blue <= MAX_BLUE)
 }
 
 fn main() {
